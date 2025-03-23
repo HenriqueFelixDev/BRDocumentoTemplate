e878a4851871b24b25ede7f8fbc4e59a4f7f8857
Henrique Felix <henriquefelixdev@gmail.com>
2025-03-23 15:36:58 -0300

    feat: cria o placeholder para o richtextbox e a lógica de inserção dele

--- BRDocumentoTemplate.UI/MainWindow.xaml.cs
+++ BRDocumentoTemplate.UI/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using BRDocumentoTemplate.UI.Repositories;
+﻿using BRDocumentoTemplate.UI.Entities;
+using BRDocumentoTemplate.UI.Repositories;
+using BRDocumentoTemplate.UI.RichTextElements;
 using BRDocumentoTemplate.UI.Views.Propriedades;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace BRDocumentoTemplate.UI;
 
@@ -21,5 +25,59 @@ public partial class MainWindow : Window
     private void buttonAdicionarPropriedade_Click(object sender, RoutedEventArgs e)
     {
         new DialogAdicionarPropriedade(propriedadeViewModel).ShowDialog();
+    }
+
+    private void menuItemAdicionarPropriedade_Click(object sender, RoutedEventArgs e)
+    {
+        if (e.OriginalSource is MenuItem { Header: PropriedadeTemplate propriedade })
+        {
+            Inline placeholder = new InlinePlaceholder(propriedade.Nome);
+
+            object parent = richText.Selection.Start.Parent;
+
+            if (parent is Paragraph paragraph)
+            {
+                paragraph.Inlines.Add(placeholder);
+                return;
+            }
+
+            if (parent is Run run)
+            {
+                richText.BeginChange();
+
+                try
+                {
+                    _AdicionarPlaceholderNoRunSelecionado(run, placeholder);
+                }
+                finally
+                {
+                    richText.EndChange();
+                }
+                
+            }
+        }
+    }
+
+    private void _AdicionarPlaceholderNoRunSelecionado(Run run, Inline placeholder)
+    {
+        if (run.Parent is Paragraph runParagraph)
+        {
+            string beforePlaceholderText = richText.Selection.Start.GetTextInRun(LogicalDirection.Backward);
+            string afterPlaceholderText = richText.Selection.End.GetTextInRun(LogicalDirection.Forward);
+
+            runParagraph.Inlines.Remove(run);
+
+            if (beforePlaceholderText.Length > 0)
+            {
+                runParagraph.Inlines.Add(new Run(beforePlaceholderText));
+            }
+
+            runParagraph.Inlines.Add(placeholder);
+
+            if (afterPlaceholderText.Length > 0)
+            {
+                runParagraph.Inlines.Add(new Run(afterPlaceholderText));
+            }
+        }
     }
 }
---
+++ BRDocumentoTemplate.UI/RichTextElements/InlinePlaceholder.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace BRDocumentoTemplate.UI.RichTextElements;
+
+public class InlinePlaceholder : InlineUIContainer
+{
+    public InlinePlaceholder(string text)
+    {
+        Child = new Border
+        {
+            CornerRadius = new CornerRadius(5),
+            Padding = new Thickness(8, 2, 8, 2),
+            Background = Brushes.Red,
+            Child = new TextBlock
+            {
+                Text = text,
+                Foreground = Brushes.White,
+            }
+        };
+    }
+}
